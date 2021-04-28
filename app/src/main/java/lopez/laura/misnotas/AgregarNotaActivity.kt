bccd0679d3ab4a241cf6d96c65b9eb62e015255f
Lauralopezfre <15325051520023@cetis108.edu.mx>
2021-04-28 11:51:27 -0600

    Se agregan los permisos y la ubicación del archivo

--- app/src/main/java/lopez/laura/misnotas/AgregarNotaActivity.kt
+++ app/src/main/java/lopez/laura/misnotas/AgregarNotaActivity.kt
@@ -1,11 +1,75 @@
 package lopez.laura.misnotas
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.w3c.dom.Text
+import java.io.FileOutputStream
+import java.lang.Exception
+import java.util.jar.Manifest
 
 class AgregarNotaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_nota)
+
+        var botonguardar: Button = findViewById(R.id.btn_guardar) as Button
+        botonguardar.setOnClickListener {
+            guardar_notas()
+        }
+    }
+
+    fun guardar_notas(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),23)
+
+        }else{
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            235 -> {
+                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+
+                }else{
+                    Toast.makeText(this, "Error: permisos denegados", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun guadar(){
+        var et_titulo: TextView = findViewById(R.id.et_titulo) as TextView
+        var et_contenido: TextView = findViewById(R.id.et_contenido) as TextView
+        var titulo = et_titulo.text.toString()
+        var cuerpo = et_contenido.text.toString()
+
+        if(titulo == "" || cuerpo == ""){
+            Toast.makeText(this, "Error: campos vacios", Toast.LENGTH_SHORT).show()
+        }else{
+            try{
+                //val archivo = File(ubicacion(), titulo + ".txt")
+                //val fos = FileOutputStream(archivo)
+                //fos.write(cuerpo.toByteArray())
+                //fos.close()
+                Toast.makeText(
+                        this,
+                        "Se guardo el archivo en la carpeta publica",
+                        Toast.LENGTH_SHORT
+                ).show()
+
+            }catch (e: Exception){
+                Toast.makeText(this, "Error, no se guardo el archivo", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
